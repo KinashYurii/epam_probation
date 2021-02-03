eaed71fbb79f8d8ac8eac6089bcbfd8f9c3cd9a1
mihay <Yfc22nehwsz$>
2021-02-03 16:18:45 +0200

    Fixed authentication. Added second query in order to get authorities

--- src/main/java/com/epam/probation/configuration/WebSecurityConfig.java
+++ src/main/java/com/epam/probation/configuration/WebSecurityConfig.java
@@ -1,15 +1,16 @@
 package com.epam.probation.configuration;
 
+import javax.sql.DataSource;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,20 +20,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private final DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select username, password"
-                        + " from users where username=?")
-                .passwordEncoder(new BCryptPasswordEncoder());
+            .usersByUsernameQuery("select name, password, 'true'"
+                + " from users where name=?")
+            .authoritiesByUsernameQuery(
+            "select name, 'ADMIN' from users " +
+                "where name=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
-                .and()
-                .httpBasic(); // Authenticate users with HTTP basic authentication
+        http
+            .authorizeRequests()
+            .antMatchers("/auth/**").permitAll(); // Authenticate users with HTTP basic authentication
     }
 }
